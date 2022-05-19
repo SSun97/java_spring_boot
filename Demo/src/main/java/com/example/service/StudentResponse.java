694b9fa4a41418b0c89eb2e23e54ea9711001b12
ssun97 <sunmingyu.97@gmail.com>
2022-05-19 09:19:06 -0500

    installed lombok and used @Data

--- Demo/src/main/java/com/example/service/StudentResponse.java
+++ Demo/src/main/java/com/example/service/StudentResponse.java
@@ -1,41 +1,25 @@
 package com.example.service;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
 public class StudentResponse {
-	
+
+	@JsonIgnore
 	private long id;
 	
+	@JsonProperty("first_name")
 	private String firstName;
-	
 	private String lastName;
 
-	public long getId() {
-		return id;
-	}
 
 	public StudentResponse(long id, String firstName, String lastName) {
 		super();
 		this.id = id;
 		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
