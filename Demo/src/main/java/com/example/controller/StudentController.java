888d30008f2ef54e46dca40df28fb83510859895
ssun97 <sunmingyu.97@gmail.com>
2022-05-19 20:36:18 -0500

    Completed DELETE method

--- Demo/src/main/java/com/example/controller/StudentController.java
+++ Demo/src/main/java/com/example/controller/StudentController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -53,8 +54,12 @@ public class StudentController {
 		
 		return new StudentResponse(student);
 	}
-	@DeleteMapping("/delete")
-	public String deleteStudent(@RequestParam long id) {
+//	@DeleteMapping("/delete")
+//	public String deleteStudent(@RequestParam long id) {
+//		return studentService.deleteStudent(id);
+//	}
+	@DeleteMapping("/delete/{id}")
+	public String deleteStudent(@PathVariable long id) {
 		return studentService.deleteStudent(id);
 	}
 	
